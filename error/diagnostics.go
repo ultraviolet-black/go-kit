acdc1ac3e4ff92cc8f4c659dcc291674600665c1
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2023-11-13 12:11:05 -0300

    Add error

---
+++ error/diagnostics.go
@@ -0,0 +1,99 @@
+package error
+
+import (
+	"context"
+	"strings"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+)
+
+type Diagnostic interface {
+	String() string
+	IsError() bool
+}
+
+type errorDiagnostic struct {
+	err error
+}
+
+func (e *errorDiagnostic) String() string {
+	return e.err.Error()
+}
+
+func (e *errorDiagnostic) IsError() bool {
+	return true
+}
+
+type messageDiagnostic struct {
+	message string
+	isError bool
+}
+
+func (e *messageDiagnostic) String() string {
+	return e.message
+}
+
+func (e *messageDiagnostic) IsError() bool {
+	return e.isError
+}
+
+type Diagnostics []Diagnostic
+
+func (d Diagnostics) AddError(err error) Diagnostics {
+
+	if err == nil {
+		return d
+	}
+
+	return append(d, &errorDiagnostic{err: err})
+
+}
+
+func (d Diagnostics) AddErrorWithMessage(err error, msg string) Diagnostics {
+
+	if err == nil {
+		return d
+	}
+
+	return append(d, &messageDiagnostic{message: msg, isError: true})
+
+}
+
+func (d Diagnostics) HasError() bool {
+
+	for _, diagnostic := range d {
+		if diagnostic.IsError() {
+			return true
+		}
+	}
+
+	return false
+
+}
+
+func (d Diagnostics) ToGrpcError(ctx context.Context, code codes.Code, requestIdHeader string) error {
+
+	errors := []string{}
+
+	for _, diagnostic := range d {
+		if diagnostic.IsError() {
+			errors = append(errors, diagnostic.String())
+		}
+	}
+
+	md, ok := metadata.FromOutgoingContext(ctx)
+	if ok {
+
+		vals, ok := md[requestIdHeader]
+
+	}
+
+	if len(errors) == 0 {
+		return nil
+	}
+
+	return grpc.Errorf(code, strings.Join(errors, "; "))
+
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.17.0
+	google.golang.org/grpc v1.58.2
 )
 
 require (
@@ -25,6 +26,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.25.0 // indirect
 	github.com/aws/smithy-go v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
@@ -40,8 +42,11 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230920204549-e6e6cdab5c13 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
